17c49e9c01fe15353b8ea895c9b013740b9c7069
Emil Kloeden <emilkloeden@gmail.com>
2020-04-04 18:18:17 +1030

    Improved Footer text

--- covid/src/components/Footer.js
+++ covid/src/components/Footer.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 function Footer() {
-    return (
-        <footer>
-            <p>Source data is provided by <a href="https://interactive.guim.co.uk/docsdata/1q5gdePANXci8enuiS4oHUJxcxC13d6bjMRSicakychE.json">The Guardian</a> under a Creative Commons Attribution 3.0 Australia (CC BY 3.0 AU).</p>
-            <p>Calculations performed on that source data are my own and highly unlikely to be accurate.</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>
+        Source data is provided by{" "}
+        <a href="https://interactive.guim.co.uk/docsdata/1q5gdePANXci8enuiS4oHUJxcxC13d6bjMRSicakychE.json">
+          The Guardian
+        </a>{" "}
+        under a Creative Commons Attribution 3.0 Australia (CC BY 3.0 AU).
+      </p>
+      <p>
+        Calculations such as "Growth Factor" have been applied subsequently.
+        Please report any issues on{" "}
+        <a href="https://github.com/emilkloeden/covid/issues">github</a>.
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
